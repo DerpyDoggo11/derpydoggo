796a26604de2eb641433c0c4669567f9a04da31b
AmazinAxel <axel@amazinaxel.com>
2025-05-08 14:42:03 -0700

    feat: page animations, better carousel

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ export const projects = [
 		title: 'Open Conflict',
 		description: 'A Roblox plane combat game (in development)',
 		image: 'open-conflict.png',
-		url: '',
+		url: 'https://discord.gg/xkWpGd7gTF'
 	},
 	{
 		title: 'ChimkenNuggz',
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,12 @@
 <script>
 	import { projects } from '../config.js'
-	import { fade } from 'svelte/transition';
+    import { fly } from 'svelte/transition';
+	import { quintOut } from 'svelte/easing';
+	const transition1 = { y: -10, duration: 700, delay: 500, easing: quintOut };
+	const transition2 = { y: -10, duration: 700, delay: 600, easing: quintOut };
+	const transition3 = { y: -10, duration: 700, delay: 900, easing: quintOut };
+	const transition4 = { y: -10, duration: 700, delay: 1200, easing: quintOut };
+
 	import emblaCarouselSvelte from 'embla-carousel-svelte';
 	import carouselClassNames from 'embla-carousel-class-names'
 
@@ -13,6 +19,9 @@
 
 	const onSlideMoved = () => 
 		currentlySelected = emblaAPI.selectedScrollSnap();
+	
+	let pageAnimation = $state(false);
+	setTimeout(() => (pageAnimation = true), 1);
 </script>
 
 <svelte:head>
@@ -24,44 +33,46 @@
 	<link rel="stylesheet" href="/css/carousel.css"/>
 </svelte:head>
 
-<main in:fade|local={{ duration: 150 }}>
-	<div id="profile">
-		<img src="/media/derpydoggo.jpeg" alt="DerpyDoggo's Profile"/>
-		<h1>DerpyDoggo</h1>
-		<h2>Awesome dev who makes games and other stuff</h2>
-	</div>
-
-	<div id="links">
-		<a href="http://discordapp.com/users/587320323762946069"><img src="/media/icons/discord.svg" alt="Discord"/></a>
-		<a href="https://www.youtube.com/@derpydoggo11"><img src="/media/icons/youtube.svg" alt="YouTube"/></a>
-		<a href="https://github.com/DerpyDoggo11"><img src="/media/icons/github.svg" alt="GitHub"/></a>
-	</div>
-
-	<!-- Projects list -->
-	<div style="position: relative;">
-		<section class="embla" use:emblaCarouselSvelte={{ options, plugins }} onemblaInit={initAPI}>
-			<div class="embla__container">
-				{#each projects as { title, image }}
-					<div class="embla__slide">
-						<img src="/media/projects/{image}" alt="Image of {title}">
-					</div>
-				{/each}
+	<main>
+		{#if pageAnimation}
+			<div id="profile" in:fly|local={transition1}>
+				<img src="/media/derpydoggo.jpeg" alt="DerpyDoggo's Profile"/>
+				<h1>DerpyDoggo</h1>
+				<h2>Awesome dev who makes games and other stuff</h2>
 			</div>
-		</section>
-		<button class="embla__prev" onclick={() => emblaAPI.scrollPrev()}><img class="noImgStyle" src="/media/icons/arrow-left.svg" alt="Left arrow"></button>
-		<button class="embla__next" onclick={() => emblaAPI.scrollNext()}><img class="noImgStyle" src="/media/icons/arrow-right.svg" alt="Right arrow"></button>
-	</div>
-	<section class="projectInfo">
-		{#if projects[currentlySelected].url}
-			<a href={projects[currentlySelected].url}><h2>{projects[currentlySelected].title}</h2></a>
-		{:else}
-			<h2>{projects[currentlySelected].title}</h2>
+
+			<div id="links" in:fly|local={transition2}>
+				<a href="http://discordapp.com/users/587320323762946069" target="_blank" rel="noopener noreferrer"><img src="/media/icons/discord.svg" alt="Discord"/></a>
+				<a href="https://www.youtube.com/@derpydoggo11" target="_blank" rel="noopener noreferrer"><img src="/media/icons/youtube.svg" alt="YouTube"/></a>
+				<a href="https://github.com/DerpyDoggo11" target="_blank" rel="noopener noreferrer"><img src="/media/icons/github.svg" alt="GitHub"/></a>
+			</div>
+
+			<!-- Projects list -->
+			<div style="position: relative;" in:fly|local={transition3}>
+				<section class="embla" use:emblaCarouselSvelte={{ options, plugins }} onemblaInit={initAPI}>
+					<div class="embla__container">
+						{#each projects as { title, image }}
+							<div class="embla__slide">
+								<img src="/media/projects/{image}" alt="Image of {title}">
+							</div>
+						{/each}
+					</div>
+				</section>
+				<button class="embla__prev" onclick={() => emblaAPI.scrollPrev()}><img class="noImgStyle" src="/media/icons/arrow-left.svg" alt="Left arrow"></button>
+				<button class="embla__next" onclick={() => emblaAPI.scrollNext()}><img class="noImgStyle" src="/media/icons/arrow-right.svg" alt="Right arrow"></button>
+			</div>
+			<section class="projectInfo" in:fly|local={transition4}>
+				{#if projects[currentlySelected].url}
+					<a href={projects[currentlySelected].url} target="_blank" rel="noopener noreferrer"><h2>{projects[currentlySelected].title}</h2></a>
+				{:else}
+					<h2>{projects[currentlySelected].title}</h2>
+				{/if}
+				<p>{@html projects[currentlySelected].description}</p>
+			</section>
 		{/if}
-		<p>{@html projects[currentlySelected].description}</p>
-	</section>
-</main>
+	</main>
 
 <footer>
-	<p>Copyright (c) {new Date().getFullYear()} DerpyDoggo • All Rights Reserved <a href="https://github.com/DerpyDoggo11/derpydoggo">(Source)</a></p>
-	<a href="https://amazinaxel.com" class="linkArrow">Brother's site</a>
+	<p>Copyright (c) {new Date().getFullYear()} DerpyDoggo • All Rights Reserved <a href="https://github.com/DerpyDoggo11/derpydoggo" target="_blank" rel="noopener noreferrer">(Source)</a></p>
+	<a href="https://amazinaxel.com" class="linkArrow" target="_blank" rel="noopener noreferrer">Made by AmazinAxel (big bro)</a>
 </footer>
