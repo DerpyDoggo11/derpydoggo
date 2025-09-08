d783855081af8361bab111846cc3ec8a9adcf668
DerpyDoggo11 <vrs18451@gmail.com>
2025-09-08 11:43:58 -0700

    updated pictures and added kingdomrts

--- src/config.js
+++ src/config.js
@@ -1,34 +1,52 @@
 export const projects = [
 	{
+		title: 'Kingdom RTS',
+		description: '[🥇 Minekeep August 2025 Gamejam Winner] Real Time Strategy Game in Minecraft (kingdomrts.minekeep.gg)',
+		image: 'kingdom-rts.png',
+		url: 'https://discord.gg/FvpeerxExV'
+	},
+	{
+		title: 'Nocturn',
+		description: 'Minecraft Murder Mystery (nocturn.minekeep.gg)',
+		image: 'nocturn.png',
+		url: 'https://nocturn.paynow.store/'
+	},
+	{
 		title: 'Open Conflict',
-		description: 'A Roblox plane combat game (in development)',
+		description: 'Plane Combat Simulator in Roblox (in development)',
 		image: 'open-conflict.png',
-		url: 'https://nocturn.paynow.store'
+		url: 'https://discord.gg/EbFtPMvFGd'
 	},
 	{
 		title: 'ChimkenNuggz',
-		description: 'A Minecraft turn-based strategy game (chimkennuggz.minekeep.gg)',
+		description: 'Turn-Based Strategy Game in Minecraft (chimkennuggz.minekeep.gg)',
 		image: 'chimkennuggz.png',
 		url: ''
 	},
 	{
-		title: 'Nocturn',
-		description: 'A Minecraft murder mystery game (nocturn.minekeep.gg)',
-		image: 'nocturn.png',
-		url: 'https://discord.gg/hJnapqFFC4'
+		title: 'Mowerbot',
+		description: 'A lawn-mower robot with integrated pathfinding and PID control (in development)',
+		image: 'mowerbot.jpg',
+		url: ''
+	},
+	{
+		title: 'Doggobot',
+		description: 'A quadruped robot utilizing inverse kinematics (in development)',
+		image: 'doggobot.jpg',
+		url: ''
 	},
 	{
 		title: 'LED Volumetric Display',
-		description: 'A persistance of vision (POV) display using an arduino, slip ring, and motor',
+		description: 'A persistance of vision (POV) display',
 		image: 'volumetric-display.png',
 		url: ''
 	},
-	{
-		title: 'Air Compressor',
-		description: 'A simple air compressor/blower for an inflatable kayak',
-		image: 'air-compressor.jpg',
-		url: ''
-	},
+	//{
+	//	title: 'Air Compressor',
+	//	description: 'A simple air compressor/blower for an inflatable kayak',
+	//	image: 'air-compressor.jpg',
+	//	url: ''
+	//},
 	{
 		title: 'Ground-Effect Plane',
 		description: 'An experimental ground-effect RC plane',
@@ -40,29 +58,18 @@ export const projects = [
 		description: 'An glider RC plane based on the <a href="https://www.instructables.com/EZ-Pelican-Durable-Easy-to-Build-and-Fly-Radio-Con/">Pelican plane</a>',
 		image: 'glider-plane.jpg',
 		url: ''
-	},
-	{
-		title: 'Reading Light',
-		description: 'An LED reading light made of plexiglass and 3D printed parts',
-		image: 'reading-light.jpg',
-		url: ''
-	},
-	{
-		title: 'Doggobot',
-		description: 'A quadruped robot utilizing inverse kinematics (in development)',
-		image: 'doggobot.jpg',
-		url: ''
-	},
-	{
-		title: 'Kayak Motor',
-		description: 'A basic trolling motor for a kayak (in development)',
-		image: 'kayak-motor.png',
-		url: ''
-	},
-	{
-		title: 'Mowerbot',
-		description: 'A lawn mower robot run by an esp32 and two hoverboard motors (in development)',
-		image: 'mowerbot.jpg',
-		url: ''
 	}
+	
+	//{
+	//	title: 'Reading Light',
+	//	description: 'An LED reading light made of plexiglass and 3D printed parts',
+	//	image: 'reading-light.jpg',
+	//	url: ''
+	//},
+	//{
+	//	title: 'Kayak Motor',
+	//	description: 'A basic trolling motor for a kayak (in development)',
+	//	image: 'kayak-motor.png',
+	//	url: ''
+	//},
 ];
